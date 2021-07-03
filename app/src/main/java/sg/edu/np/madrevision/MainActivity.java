f9f2096e692493c93b7823da9c2ef368f6bf351f
lim44104 <s10207455@connect.np.edu.sg>
2021-07-03 15:28:36 +0800

    Add hello try

--- app/src/main/java/sg/edu/np/madrevision/MainActivity.java
+++ app/src/main/java/sg/edu/np/madrevision/MainActivity.java
@@ -22,8 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("Debug","create");
 
+        //Hello Try
         Intent rec = getIntent();
         int value = rec.getIntExtra("id",0);
+        Log.d(TAG, "position= " + value);
         /* practical 2
         //name and description are loaded from the User object.
         u = new User();
